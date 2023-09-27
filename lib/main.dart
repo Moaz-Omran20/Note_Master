1e56c8fd5ff5853a60eea4620c153cc266286dce
Moaz <moazomran20@gmail.com>
2023-09-27 22:53:54 +0300

    create notes box

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:note/views/note_view.dart';
 
-void main() {
-  runApp(const NoteMaster());
+import 'constants/hive.dart';
+
+void main() async{
+  await Hive.initFlutter();
+  await Hive.openBox(kNotesBox);
+  runApp( const NoteMaster());
 }
 
 
