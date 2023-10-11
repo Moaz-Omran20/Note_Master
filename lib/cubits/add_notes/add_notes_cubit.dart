258ac477b58526929f40dad71b917678f7ae78f7
Moaz <moazomran20@gmail.com>
2023-10-11 18:09:37 +0300

    provide bloc to app

--- lib/cubits/add_notes/add_notes_cubit.dart
+++ lib/cubits/add_notes/add_notes_cubit.dart
@@ -1,5 +1,7 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
+import 'package:note/constants/hive.dart';
 import 'package:note/models/note_model.dart';
 
 part 'add_notes_state.dart';
@@ -7,10 +9,13 @@ part 'add_notes_state.dart';
 class AddNotesCubit extends Cubit<AddNotesState> {
   AddNotesCubit() : super(AddNotesInitial());
 
-
-  addNote(NoteModel note)
-  {
-
-
+  addNote(NoteModel note) async {
+    emit(AddNotesLoading());
+    try {
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+      await notesBox.add(note);
+    } catch (e) {
+      AddNotesFailure(e.toString());
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:note/cubits/add_notes/add_notes_cubit.dart';
 import 'package:note/models/note_model.dart';
 import 'package:note/views/note_view.dart';
 
 import 'constants/hive.dart';
 
-void main() async{
+void main() async {
   await Hive.initFlutter();
   await Hive.openBox(kNotesBox);
   Hive.registerAdapter(NoteModelAdapter());
-  runApp( const NoteMaster());
+  runApp(const NoteMaster());
 }
-
 
 class NoteMaster extends StatelessWidget {
   const NoteMaster({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(brightness: Brightness.dark,fontFamily: 'Poppins'),
-      home: const NotesView(),
-
-    ) ;
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => AddNotesCubit(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
+        home: const NotesView(),
+      ),
+    );
   }
 }
-
