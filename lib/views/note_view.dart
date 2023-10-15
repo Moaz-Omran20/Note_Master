1ec6aa30fdb9d4eabfde51f75aa33fa24da8faae
Moaz <moazomran20@gmail.com>
2023-10-15 17:23:31 +0300

    Adding AddNotesCubit to bottom sheet

--- lib/views/note_view.dart
+++ lib/views/note_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:note/cubits/add_notes/add_notes_cubit.dart';
 import 'package:note/views/widgets/add_note.dart';
 import 'package:note/views/widgets/notes_view_body.dart';
 
@@ -11,17 +14,33 @@ class NotesView extends StatelessWidget {
         floatingActionButton: FloatingActionButton(
             onPressed: () {
               showModalBottomSheet(
-                shape:  RoundedRectangleBorder(
+                shape: const RoundedRectangleBorder(
                     borderRadius: BorderRadius.only(
                         topRight: Radius.circular(18),
                         topLeft: Radius.circular(18))),
                 context: context,
                 builder: (context) {
-                  return AddNote();
+                  return BlocConsumer<AddNotesCubit, AddNotesState>(
+                    listener: (context, state) {
+                      if(state is AddNotesSuccess)
+                        {
+                          Navigator.pop(context);
+                        }
+                      if(state is AddNotesFailure)
+                        {
+                          print("Note added successfully");
+                        }
+                    },
+                    builder: (context, state) {
+                      return ModalProgressHUD(
+                          inAsyncCall: state is AddNotesLoading ? true : false,
+                          child: const AddNote());
+                    },
+                  );
                 },
               );
             },
-            child: Icon(Icons.add)),
-        body: NotesViewBody());
+            child: const Icon(Icons.add)),
+        body: const NotesViewBody());
   }
 }
