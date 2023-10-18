08d4fcb4e8a1486d59a60a9808fc49baf3091e26
Moaz <moazomran20@gmail.com>
2023-10-18 16:38:57 +0300

    note colors added

--- lib/views/widgets/add_note.dart
+++ lib/views/widgets/add_note.dart
@@ -59,33 +59,101 @@ class _AddNoteFormState extends State<AddNoteForm> {
             hintText: "SubTitle",
             maxLines: 6,
           ),
+          ColorsListView(),
           const SizedBox(
             height: 20,
           ),
-          CustomButton(onTap: () {
-            if (formKey.currentState!.validate()) {
-              formKey.currentState!.save();
+          CustomButton(
+            onTap: () {
+              if (formKey.currentState!.validate()) {
+                formKey.currentState!.save();
 
-              var currentDate=DateTime.now();
-              var formatedDate = DateFormat.yMMMEd().format(currentDate);
-              var noteModel = NoteModel(title: title!,
-                  content: subtitle!,
-                  date: formatedDate.toString(),
-                  color: Colors.blue.value);
-              BlocProvider.of<AddNotesCubit>(context).addNote(noteModel);
-            }
-            else {
-              autovalidateMode = AutovalidateMode.always;
-              setState(() {
-
-              });
-            }
-          },),
+                var currentDate = DateTime.now();
+                var formatedDate = DateFormat.yMMMEd().format(currentDate);
+                var noteModel = NoteModel(
+                    title: title!,
+                    content: subtitle!,
+                    date: formatedDate.toString(),
+                    color: Colors.blue.value);
+                BlocProvider.of<AddNotesCubit>(context).addNote(noteModel);
+              } else {
+                autovalidateMode = AutovalidateMode.always;
+                setState(() {});
+              }
+            },
+          ),
           const SizedBox(
             height: 20,
           ),
         ],
       ),
+    );
+  }
+}
+
+class ColorItem extends StatelessWidget {
+  bool isActive;
+  Color color;
+
+  ColorItem({required this.isActive, required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return isActive
+        ?  CircleAvatar(
+            radius: 40,
+            backgroundColor: Colors.white,
+            child: CircleAvatar(
+              radius: 28,
+              backgroundColor: color,
+            ),
+          )
+        : CircleAvatar(
+            radius: 35,
+            backgroundColor: color,
+          );
+  }
+}
+
+class ColorsListView extends StatefulWidget {
+  @override
+  State<ColorsListView> createState() => _ColorsListViewState();
+}
+
+class _ColorsListViewState extends State<ColorsListView> {
+  bool? isActive;
+  int currentIndex = 0;
+  List<Color> colors = [
+    Color(0xFF219C90),
+    Color(0xFFE9B824),
+    Color(0xFFEE9322),
+    Color(0xFFD83F31),
+    Color(0xFFA7D397),
+    Color(0xFF016A70),
+    Color(0xFFD2DE32),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 35 * 2,
+      child: ListView.builder(
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: EdgeInsets.only(top: 10),
+              child: GestureDetector(
+                  onTap: () {
+                    currentIndex = index;
+                    setState(() {});
+                  },
+                  child: ColorItem(
+                    isActive: currentIndex == index,
+                    color: colors[index],
+                  )),
+            );
+          },
+          itemCount: colors.length,
+          scrollDirection: Axis.horizontal),
     );
   }
 }
